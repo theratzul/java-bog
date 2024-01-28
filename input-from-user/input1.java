95f8e6bc0402261fbba022782cfa200d46148045
Bogdan <bogdan@localhost.localdomain>
2024-01-28 21:12:01 +0200

    feature/java-scanner

---
+++ input-from-user/input1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class input1 {
+    public static void main(String[] args) {
+        Scanner bog = new Scanner(System.in);
+        System.out.println(bog.nextLine());
+    }
+}
